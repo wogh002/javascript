efe37413264b339fee2512dd53a283154d416321
wogh002 <ekekekem159@gmail.com>
2021-05-25 23:49:27 +0900

    study props,list key

--- dream_coding/react/react-basic/habit-tracker/src/components/habit.jsx
+++ dream_coding/react/react-basic/habit-tracker/src/components/habit.jsx
@@ -14,10 +14,12 @@ class Habit extends Component {
         this.setState({ count: count < 0 ? 0 : count });
     }
     render() {
+        // props = 외부 컴포넌트에서 전달받은 propreties
+        const { name, count } = this.props.habitProps;
         return (
             <li className='habit'>
-                <span className='habit-name'>Reading</span>
-                <span className='habit-count'>{this.state.count}</span>
+                <span className='habit-name'>{name}</span>
+                <span className='habit-count'>{count}</span>
                 <button className='habit-button habit-increase' onClick={this.handleIncrement}><i className="fas fa-plus-square"></i></button>
                 <button className='habit-button habit-decrease' onClick={this.handleDecrement}><i className="fas fa-minus-square"></i></button>
                 <button className='habit-button habit-delete'><i className="fas fa-trash"></i></button>
--- dream_coding/react/react-basic/habit-tracker/src/components/habits.jsx
+++ dream_coding/react/react-basic/habit-tracker/src/components/habits.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import Habit from './components/habit';
+import Habit from './habit';
 class Habits extends Component {
     state = {
         habits: [
-            { name: 'Reading', count: 0 },
-            { name: 'Running', count: 0 },
-            { name: 'Coding', count: 0 },
+            { id: 1, name: 'Reading', count: 0 },
+            { id: 2, name: 'Running', count: 0 },
+            { id: 3, name: 'Coding', count: 0 },
         ]
     }
     render() {
         return (
             <ul>
                 {
-                    this.state.habits.map(habit => <Habit />)
+                    this.state.habits.map(habit => (
+                        // props란? 외부컴포넌트로 데이터를 전달해준다.
+                        <Habit key={habit.id} habitProps={habit} />
+                    ))
                 }
             </ul>
         );
