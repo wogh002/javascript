73d70174cec298b80d87c2559422a390539ecbff
wogh002 <ekekekem159@gmail.com>
2021-05-26 17:46:57 +0900

    spread
    리액트에서는 직접적으로 state를 변경하는 것은 좋지 않다.

--- dream_coding/react/react-basic/habit-tracker/src/components/habit.jsx
+++ dream_coding/react/react-basic/habit-tracker/src/components/habit.jsx
@@ -3,26 +3,25 @@ import React, { Component } from 'react';
 class Habit extends Component {
     //컴포넌트란? 데이터를 입력받아서 dom NODE 를 출력하는 함수.
     // 컴포넌트에는 state라는 멤버변수 객체가 존재한다.
-    state = {
-        count: 0
-    };
     handleIncrement = () => {
-        this.setState({ count: this.state.count + 1 });
+        this.props.onIncrement(this.props.habit);
     }
     handleDecrement = () => {
-        const count = this.state.count - 1;
-        this.setState({ count: count < 0 ? 0 : count });
+        this.props.onDecrement(this.props.habit);
+    }
+    handleDelete = () => {
+        this.props.onDelete(this.props.habit);
     }
     render() {
         // props = 외부 컴포넌트에서 전달받은 propreties
-        const { name, count } = this.props.habitProps;
+        const { name, count } = this.props.habit;
         return (
             <li className='habit'>
                 <span className='habit-name'>{name}</span>
                 <span className='habit-count'>{count}</span>
                 <button className='habit-button habit-increase' onClick={this.handleIncrement}><i className="fas fa-plus-square"></i></button>
                 <button className='habit-button habit-decrease' onClick={this.handleDecrement}><i className="fas fa-minus-square"></i></button>
-                <button className='habit-button habit-delete'><i className="fas fa-trash"></i></button>
+                <button className='habit-button habit-delete' onClick={this.handleDelete}><i className="fas fa-trash"></i></button>
             </li>
         );
     }
--- dream_coding/react/react-basic/habit-tracker/src/components/habits.jsx
+++ dream_coding/react/react-basic/habit-tracker/src/components/habits.jsx
@@ -8,13 +8,38 @@ class Habits extends Component {
             { id: 3, name: 'Coding', count: 0 },
         ]
     }
+    handleIncrement = habit => {
+        // 리액트에서 직접적으로 state 를 변경하는 것은 좋지 않다.
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        habits[index].count++;
+        this.setState({ habits });
+    }
+    handleDecrement = habit => {
+        // 리액트에서 직접적으로 state 를 변경하는 것은 좋지 않다.
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        const count = habits[index].count - 1;
+        habits[index].count = count < 0 ? 0 : count;
+        this.setState({ habits });
+    }
+    handleDelete = habit => {
+        const habits = this.state.habits.filter(item => item.id !== habit.id);
+        this.setState({ habits });
+    }
     render() {
         return (
             <ul>
                 {
                     this.state.habits.map(habit => (
-                        // props란? 외부컴포넌트로 데이터를 전달해준다.
-                        <Habit key={habit.id} habitProps={habit} />
+                        // props란? 자식컴포넌트로 데이터를 전달해준다.
+                        <Habit
+                            key={habit.id}
+                            habit={habit}
+                            onIncrement={this.handleIncrement}
+                            onDecrement={this.handleDecrement}
+                            onDelete={this.handleDelete}
+                        />
                     ))
                 }
             </ul>
